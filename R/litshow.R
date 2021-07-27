eff64f95c38c1cbd48d73a46c0888f647bcd43bd
ZhonghuiGai <zhgai@aliyun.com>
2021-07-27 12:23:44 +0800

    version 0.0.1

--- R/litshow.R
+++ R/litshow.R
@@ -2,6 +2,7 @@
 #'
 #' @param litsence the result of litsence function
 #' @param n display number
+#' @param size the text size
 #'
 #' @return a plain text
 #' @export
@@ -10,21 +11,26 @@
 #' @examples
 #' res <- litsence(keywords = "gut microbiota")
 #' litshow(res, 1:30)
-litshow <- function(litsence, n = 1:30, size = 15){
+litshow <- function(litsence, n = 1:30, size = 15,
+                    color = FALSE){
   res <- data.frame(row.names(litsence), litsence)
   res <- res[n, ]
   colnames(res) <- NULL
   m <- nrow(res)
   m <- 1:m
-  res  |>  kableExtra::kable('html')  |>
-    kableExtra::kable_styling(font_size = size)  |>
-    kableExtra::row_spec(m[m %% 2 != 0], bold = T, color = "white",
-             background = "#D7261E80")  |>
-    kableExtra::row_spec(m[m %% 2 == 0], bold = T, color = "black",
-             background = "#00bbff30")  |>
-    kableExtra::column_spec(1, bold = T, color = "black", italic = T,
-                background = "#00bbff30", underline = T,
-                border_right = T, border_left = T)
+  tab <- res  |>  kableExtra::kable('html')  |>
+    kableExtra::kable_styling(font_size = size)
+  if (color) {
+    tab <- tab  |>
+      kableExtra::row_spec(m[m %% 2 != 0], bold = T, color = "white",
+                           background = "#D7261E80")  |>
+      kableExtra::row_spec(m[m %% 2 == 0], bold = T, color = "black",
+                           background = "#00bbff30")  |>
+      kableExtra::column_spec(1, bold = T, color = "black", italic = T,
+                              background = "#00bbff30", underline = T,
+                              border_right = T, border_left = T)
+  }
+  return(tab)
 }
 
 
