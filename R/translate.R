76c6c1aa8f14c09fa8523f0feef6c56d9e5c1672
ZhonghuiGai <zhgai@aliyun.com>
2021-08-01 19:24:03 +0800

    version 0.0.1

--- R/translate.R
+++ R/translate.R
@@ -17,12 +17,9 @@ translate <- function(input) {
   stopifnot(n.char[n] < 5000)
   pattern <- grepl(pattern = "[\u4e00-\u9fa5]", input[1]) # find the Chinese character
   if (n > 1) input <- do.call("paste", list(input, collapse = " a_._a "))
-  if (pattern) {
-    quary <- paste0("trans -b :en ", "\"", input, "\"")
-  } else {
-    quary <- paste0("trans -b :zh ", "\"", input, "\"")
-  }
+  lang <- ifelse(pattern, "en ", "zh ")
+  quary <- paste0("trans -b -j :", lang, input)
   trans.out <- system(quary, intern = TRUE)
-  trans.out <- strsplit(trans.out, split = "a_._a")[[1]]
+  trans.out <- strsplit(trans.out, split = "a_._a")[[1]][]
   return(trans.out)
 }
